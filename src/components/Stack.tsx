1d0c150c171bbe5c484e7ec3d060580cbb08eb2a
Jakub Pałka <jakub.palka97@gmail.com>
2025-07-24 18:25:17 +0200

    changing img card

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -66,9 +66,10 @@ export default function Stack({
 			? cardsData
 			: [
 					{ id: 1, img: 'https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?q=80&w=500&auto=format' },
-					{ id: 2, img: 'https://yelonmedia.s3.us-east-1.amazonaws.com/Modowe/DSC07926.jpg' },
-					{ id: 3, img: 'https://yelonmedia.s3.us-east-1.amazonaws.com/Motoryzacja/DSC01517.jpg' },
-					{ id: 4, img: 'https://yelonmedia.s3.us-east-1.amazonaws.com/Motoryzacja/DSC01517.jpg' },
+					{ id: 2, img: 'https://yelonmedia.s3.us-east-1.amazonaws.com/Motoryzacja/DSC01803.jpg' },
+					{ id: 3, img: 'https://yelonmedia.s3.us-east-1.amazonaws.com/Gastro/_DSC2877.jpg' },
+					{ id: 4, img: 'https://yelonmedia.s3.us-east-1.amazonaws.com/Salony+Fryzjerskie/DSC06992.jpg' },
+					{ id: 5, img: 'https://yelonmedia.s3.us-east-1.amazonaws.com/Studia+Tattoo/DSC05475.jpg' },
 			  ]
 	)
 
